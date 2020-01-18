6aa70d9da2af07cbf3779216d01f51ff958fac97
Wildney <wildarte10@gmail.com>
2020-01-18 08:24:37 -0300

    Commit de algumas imagens na pasta img, arquivos aula25.html ao aula30.html e uma pequena alteraçao no arquivo main.js

--- EstudoBootStrap/js/main.js
+++ EstudoBootStrap/js/main.js
@@ -1,3 +1,19 @@
 $(function(){
     //equivalente ao loading da página
+
+    $("#modal01").modal("show");
+
+    setTimeout(function(){
+        $("#modal01").modal("hide")
+    }, 3000);
+
+    $(".ttp").tooltip({
+        animation:false,
+        delay: {show: 1000, hide:5000}
+    });
+
+    $(".ppv").popover();
+
+    $(".btnjs").button();
+
 });
